d777332fc5eabb74a4637bb2745a59f721151bf5
Julio Cesar <julinn_cesar@hotmail.com>
2019-08-18 22:44:17 -0300

    exemplo de como usar mso

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let app = require('express')();
 let bodyParser = require('body-parser');
 let consign = require('consign')({verbose: false});
 const nodemailer = require("nodemailer");
-const handlebars = require('handlebars');
+// const handlebars = require('handlebars');
 const fs = require('fs');
 
 require('./settings/db');
@@ -26,25 +26,28 @@ let source = fs.readFileSync('./template.handlebars', 'utf8');
 // let template = handlebars.compile(source);
 
 let transporter = nodemailer.createTransport({
-    host: 'email-smtp.us-east-1.amazonaws.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-        user: 'AKIAIBLQTRXZ52KKZ23A', // generated ethereal user
-        pass: 'Ak4uMRZ9k9arVs3dw5ZcrALOpS8myItGi3Qwtza134Az' // generated ethereal password
+        user: 'julinn_cesar@hotmail.com', // generated ethereal user
+        pass: 'julio1010' // generated ethereal password
     }
 });
 
 app.get('/teste', async (req, res) => {
-    await transporter.sendMail({
-        from: 'sgf@fundacaoitausocial.org.br', // sender address
-        to: 'julinn_cesar@hotmail.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: source
-    });
-
-    return res.json({ teste: 'E-mail enviado com sucesso' });
+    try {
+        await transporter.sendMail({
+            from: 'julinn_cesar@hotmail.com', // sender address
+            to: 'julinn_cintra@hotmail.com', // list of receivers
+            subject: 'Hello ✔', // Subject line
+            text: 'Hello world?', // plain text body,
+            html: source
+        });
+        return res.json({ teste: 'E-mail enviado com sucesso' });
+    } catch (e) {
+        return res.json(e);
+    }
 });
 
 consign
